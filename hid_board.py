9cc640857d69409383318b8b579e46345e50d4ea
NicGdA <nicgda@nic.ngda.ca>
2022-12-27 01:32:45 -0500

    Multi kebs

--- hid_board.py
+++ hid_board.py
@@ -10,6 +10,12 @@ import functools
 
 import hid
 
+class kb_ids:
+    def __init__(self, vid:int, pid:int, usage_page:int, usage_id:int):
+        self.vid = vid
+        self.pid = pid
+        self.usage_page = usage_page
+        self.usage_id = usage_id
 
 # HID Nic's Commands
 HNC_ON = b'\x01'
@@ -18,11 +24,24 @@ HNC_SET = b'\x03'
 HNC_GET = b'\x04'
 HNC_SAVE = b'\x05'
 
-# GMMK Pro rev1 ANSI
-vid = 0x320F
-pid = 0x5044
-usage_page = 0xFF60
-usage_id = 0x61
+keyboards_hid_ids = {
+    # GMMK Pro rev1 ANSI
+    'gmmk_pro':
+        kb_ids(
+            vid=0x320F,
+            pid=0x5044,
+            usage_page=0xFF60,
+            usage_id=0x61
+        ),
+    # Idobao ID80v2
+    'ID80v2':
+        kb_ids(
+            vid=0x6964,
+            pid=0x0080,
+            usage_page=0xFF60,
+            usage_id=0x61
+        ),
+}
 
 device_path = None
 
@@ -35,19 +54,22 @@ group_args.add_argument('--set', help='Set the HSV values', type=functools.parti
 group_args.add_argument('--save', help='Save current HSV values', action='store_true')
 args = parser.parse_args()
 
-devices = hid.enumerate(vid, pid)
-if not len(devices):
-    print('Device not present')
-    exit(0)
-for device in devices:
-    # print(device)
-    if (
-        device['vendor_id'] == vid
-        and device['product_id'] == pid
-        and device['usage'] == usage_id
-        and device['usage_page'] == usage_page
-    ):
-        device_path = device['path']
+for keys in keyboards_hid_ids.values():
+    devices = hid.enumerate(keys.vid, keys.pid)
+    if not len(devices):
+        print('Device not present')
+        continue
+    for device in devices:
+        # print(device)
+        if (
+            device['vendor_id'] == keys.vid
+            and device['product_id'] == keys.pid
+            and device['usage'] == keys.usage_id
+            and device['usage_page'] == keys.usage_page
+        ):
+            device_path = device['path']
+            break
+    if device_path is not None:
         break
 if device_path is None:
     print('Device not present')
