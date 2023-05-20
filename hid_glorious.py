d74711f3da6fa3920565d0f1cba50fdb06a9556d
Nicolas Gros d'Aillon <nicgda@nic.ngda.ca>
2023-05-19 21:06:39 -0400

    Adding support for most of the mouse led effects

--- hid_glorious.py
+++ hid_glorious.py
@@ -24,11 +24,16 @@ class GloriousEffect(IntEnum):
     BREATHING = 3           # Pulse with colour change
     TAIL = 4                # Front to back with colour change
     SEAMLESS_BREATHING = 5  # Colour rotation
-    CONSTANT_GRB = 6        # Not in Model O
+    CONSTANT_RGB = 6        # Six fixed colours
     RAVE = 7                # Flashing different colours
-    RANDOM = 8              # Not in model O
+    RANDOM = 8              # Random led colours
     WAVE = 9                # Rainbow back to front
     SINGLE_BREATHING = 10   # Pulse single colour
+
+
+class EffectDirection(IntEnum):
+    FINGERS_TO_PALM = 0
+    PALM_TO_FINGERS = 1
 
 
 model_O_ids = MouseIds(vid=0x258A, pid=0x0036, usage_page=0xFF00, usage_id=1)
@@ -39,8 +44,18 @@ class GloriousModelORecord:
         self.__raw = bytearray(copy.copy(bytes_data))
 
     def __str__(self) -> str:
-        res = f'Mode({GloriousEffect(self.effect).name})'
-        res += f', SingleRGB({self.single_rgb:06X})'
+        res = 'Glorious Model O configuration:'
+        res += f'\n\tMode({GloriousEffect(self.effect).name})'
+        res += f'\n\tGlorious({self.glorious_direction.name}, {self.glorious_speed})'
+        res += f'\n\tSingle RGB({self.single_rgb:06X}, {self.single_rgb_brightness})'
+        res += f'\n\tBreathing({[f"{rgb:06X}" for rgb in self.breath_rgbs]}, {self.breath_speed})'
+        res += f'\n\tTail({self.tail_brightness}, {self.tail_speed})'
+        res += f'\n\tSeamless Breathing({self.seamless_speed})'
+        res += f'\n\tSix RGB({[f"{rgb:06X}" for rgb in self.constant_rgbs]})'
+        res += f'\n\tRave({[f"{rgb:06X}" for rgb in self.rave_rgbs]}, {self.rave_brightness}, {self.rave_speed})'
+        res += f'\n\tRandom({self.random_speed})'
+        res += f'\n\tWave({self.wave_brightness}, {self.wave_speed})'
+        res += f'\n\tBreathing mono({self.single_breath_rgb:06X}, {self.single_breath_speed})'
         return res
 
     @property
@@ -57,16 +72,281 @@ class GloriousModelORecord:
         self.__mark_changed()
 
     @property
+    def glorious_direction(self) -> EffectDirection:
+        """
+        Glorious effect direction of the colour change.
+        """
+        return EffectDirection(self.__raw[0x37])
+
+    @glorious_direction.setter
+    def glorious_direction(self, direction: EffectDirection):
+        """
+        Glorious effect direction of the colour change.
+        """
+        self.__raw[0x37] = direction.value
+        self.__mark_changed()
+
+    @property
+    def glorious_speed(self) -> int:
+        """
+        Value between 1 and 4.
+        """
+        return self.__raw[0x36] & 0x0F
+
+    @glorious_speed.setter
+    def glorious_speed(self, speed_1_4: int):
+        """
+        Glorious effect direction of the colour change.
+        """
+        self.__raw[0x36] = _compact_brightness_speed(4, speed_1_4)
+        self.__mark_changed()
+
+    @property
     def single_rgb(self) -> int:
         # RBG to RGB
-        return self.__raw[0x39] << 16 | self.__raw[0x3B] << 8 | self.__raw[0x3A]
+        return _rbg_to_rgb(self.__raw[0x39:0x3C])
 
     @single_rgb.setter
     def single_rgb(self, rgb: int):
         # RGB to RBG
-        self.__raw[0x39:0x3C] = [(rgb >> 16) & 0xFF, rgb & 0xFF, (rgb >> 8) & 0xFF]
+        self.__raw[0x39:0x3C] = _rgb_to_rbg(rgb)
+        self.__mark_changed()
+
+    @property
+    def single_rgb_brightness(self) -> int:
+        return self.__raw[0x38] >> 4
+
+    @single_rgb_brightness.setter
+    def single_rgb_brightness(self, brightness_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x38] = _compact_brightness_speed(brightness_1_4, 0)
+        self.__mark_changed()
+
+    @property
+    def breath_speed(self) -> int:
+        return self.__raw[0x3C] & 0x0F
+
+    @breath_speed.setter
+    def breath_speed(self, speed_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x3C] = _compact_brightness_speed(4, speed_1_4)
+        self.__mark_changed()
+
+    @property
+    def breath_rgbs(self) -> list[int]:
+        """
+        Upto seven RGB values for breathing effect
+        """
+        count = self.__raw[0x3D]
+        values = []
+        base_addr = 0x3E
+        for _ in range(count):
+            values.append(_rbg_to_rgb(self.__raw[base_addr:base_addr + 3]))
+            base_addr += 3
+        return values
+
+    @breath_rgbs.setter
+    def breath_rgbs(self, rgbs: list[int] | tuple[int, ...]) -> None:
+        """
+        Maximum of seven values.
+        """
+        if 0 < len(rgbs) <= 7:
+            self.__raw[0x3D] = len(rgbs)
+            base_addr = 0x3E
+            for value in rgbs:
+                self.__raw[base_addr:(base_addr + 3)] = _rgb_to_rbg(value)
+                base_addr += 3
+            self.__mark_changed()
+
+    @property
+    def tail_brightness(self) -> int:
+        return self.__raw[0x53] >> 4
+
+    @tail_brightness.setter
+    def tail_brightness(self, brightness_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x53] = _compact_brightness_speed(brightness_1_4, self.tail_speed)
+        self.__mark_changed()
+
+    @property
+    def tail_speed(self) -> int:
+        return self.__raw[0x53] & 0x0F
+
+    @tail_speed.setter
+    def tail_speed(self, speed_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x53] = _compact_brightness_speed(self.tail_brightness, speed_1_4)
+        self.__mark_changed()
+
+    @property
+    def seamless_speed(self) -> int:
+        return self.__raw[0x54] & 0x0F
+
+    @seamless_speed.setter
+    def seamless_speed(self, speed_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x54] = _compact_brightness_speed(4, speed_1_4)
+        self.__mark_changed()
+
+    @property
+    def constant_rgbs(self) -> list[int]:
+        # RBG to RGB
+        values = []
+        base_addr = 0x56
+        for _ in range(6):
+            values.append(_rbg_to_rgb(self.__raw[base_addr:base_addr + 3]))
+            base_addr += 3
+        return values
+
+    @constant_rgbs.setter
+    def constant_rgbs(self, six_rgbs: list[int] | tuple[int, int, int, int, int, int]) -> None:
+        """
+        The brightness and speed don't have any effects.
+        """
+        if len(six_rgbs) == 6:
+            self.__raw[0x55] = 0  # Original value from the mouse
+            base_addr = 0x56
+            for value in six_rgbs:
+                self.__raw[base_addr:(base_addr + 3)] = _rgb_to_rbg(value)
+                base_addr += 3
+            self.__mark_changed()
+
+    @property
+    def rave_brightness(self) -> int:
+        return self.__raw[0x74] >> 4
+
+    @rave_brightness.setter
+    def rave_brightness(self, brightness_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x74] = _compact_brightness_speed(brightness_1_4, self.rave_speed)
+        self.__mark_changed()
+
+    @property
+    def rave_speed(self) -> int:
+        return self.__raw[0x74] & 0x0F
+
+    @rave_speed.setter
+    def rave_speed(self, speed_1_3: int):
+        """
+        Value between 1 and 3.
+        """
+        self.__raw[0x74] = _compact_brightness_speed(self.rave_brightness, speed_1_3, max_speed=3)
+        self.__mark_changed()
+
+    @property
+    def rave_rgbs(self) -> list[int]:
+        """
+        Two RGB values
+        """
+        return [
+            _rbg_to_rgb(self.__raw[0x75:0x78]),
+            _rbg_to_rgb(self.__raw[0x78:0x7B])
+        ]
+
+    @rave_rgbs.setter
+    def rave_rgbs(self, rgbs: list[int] | tuple[int, int]) -> None:
+        """
+        Maximum of seven values.
+        """
+        if len(rgbs) == 2:
+            base_addr = 0x75
+            for value in rgbs:
+                self.__raw[base_addr:(base_addr + 3)] = _rgb_to_rbg(value)
+                base_addr += 3
+            self.__mark_changed()
+
+    @property
+    def random_speed(self) -> int:
+        return self.__raw[0x7B] & 0x0F
+
+    @random_speed.setter
+    def random_speed(self, speed_1_3: int):
+        """
+        Value between 1 and 3.
+        """
+        self.__raw[0x7B] = _compact_brightness_speed(0, speed_1_3, max_speed=3)
+        self.__mark_changed()
+
+    @property
+    def wave_brightness(self) -> int:
+        return self.__raw[0x7C] >> 4
+
+    @wave_brightness.setter
+    def wave_brightness(self, brightness_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x7C] = _compact_brightness_speed(brightness_1_4, self.wave_speed)
+        self.__mark_changed()
+
+    @property
+    def wave_speed(self) -> int:
+        return self.__raw[0x7C] & 0x0F
+
+    @wave_speed.setter
+    def wave_speed(self, speed_1_3: int):
+        """
+        Value between 1 and 3.
+        """
+        self.__raw[0x74C] = _compact_brightness_speed(self.wave_brightness, speed_1_3, max_speed=3)
+        self.__mark_changed()
+
+    @property
+    def single_breath_speed(self) -> int:
+        return self.__raw[0x7D] & 0x0F
+
+    @single_breath_speed.setter
+    def single_breath_speed(self, speed_1_4: int):
+        """
+        Value between 1 and 4.
+        """
+        self.__raw[0x7D] = _compact_brightness_speed(0, speed_1_4)
+        self.__mark_changed()
+
+    @property
+    def single_breath_rgb(self) -> int:
+        # RBG to RGB
+        return _rbg_to_rgb(self.__raw[0x7E:0x81])
+
+    @single_breath_rgb.setter
+    def single_breath_rgb(self, rgb: int):
+        # RGB to RBG
+        self.__raw[0x7E:0x81] = _rgb_to_rbg(rgb)
         self.__mark_changed()
 
     def __mark_changed(self) -> None:
         self.__raw[0x03] = 0x7B  # 123 for the write "command". 0 when reading.
 
+
+def _rgb_to_rbg(rgb: int) -> list[int]:
+    return [(rgb >> 16) & 0xFF, rgb & 0xFF, (rgb >> 8) & 0xFF]
+
+
+def _rbg_to_rgb(rbg_list: bytearray) -> int:
+    return rbg_list[0] << 16 | rbg_list[2] << 8 | rbg_list[1]
+
+
+def _limit_1_max(value: int, max_value: int) -> int:
+    if max_value > 15:
+        max_value = 15  # Safety
+    if value < 1:
+        return 1
+    elif value > max_value:
+        return max_value
+    return value
+
+
+def _compact_brightness_speed(brightness_1_4: int, speed_1_4: int, max_brightness: int = 4, max_speed: int = 4) -> int:
+    return _limit_1_max(brightness_1_4, max_brightness) << 4 | _limit_1_max(speed_1_4, max_speed)
--- hid_mouse.py
+++ hid_mouse.py
@@ -8,6 +8,7 @@ Many thanks to Samuel Čavoj for doing the reverse engineering job
 and sharing it at https://rustrepo.com/repo/sammko-gloryctl-rust-utilities
 
 This code used the hid package, using the hidapi library.
+It also needs to run as root, until I found a better solution.
 
 On macOS, hidapi is available through brew.
 """
@@ -15,17 +16,32 @@ import argparse
 
 import hid
 
-from hid_glorious import (GloriousEffect,
+from hid_glorious import (EffectDirection,
+                          GloriousEffect,
                           GloriousModelORecord,
                           model_O_ids)
 
 device_path = None
 
 parser = argparse.ArgumentParser()
-group_args = parser.add_mutually_exclusive_group(required=True)
-group_args.add_argument('--on', help='Enable led notification', action='store_true')
-group_args.add_argument('--off', help='Disable led notification', action='store_true')
-args = parser.parse_args()
+group_args = parser.add_mutually_exclusive_group(required=False)
+group_args.add_argument('--off', help='Disable led', action='store_true')
+group_args.add_argument('--glorious', help='Glorious effect, opt: direction (0/1), speed (1-4)', action='store_true')
+group_args.add_argument('--single', help='Single fix colour, opt: RGB hex value, brightness (1-4)', action='store_true')
+group_args.add_argument('--breath', help='Breathing with custom colours, opt: speed (1-4), upto seven RGB hex values', action='store_true')
+group_args.add_argument('--tail', help='Random finger to palm, opt: brightness (1-4), speed (1-4)', action='store_true')
+group_args.add_argument('--seamless', help='Seamless breathing. opt speed (1-4)', action='store_true')
+group_args.add_argument('--six', help='Six fixed colours', action='store_true')
+group_args.add_argument('--rave', help='Two colours rave, opt: brightness (1-4), speed (1-3), two RGB vex values ', action='store_true')
+group_args.add_argument('--random', help='Random changing colours on each led, opt: speed (1-3)', action='store_true')
+group_args.add_argument('--wave', help='Colour wave from palm to finger, opt: brightness (1-4), speed (1-3)', action='store_true')
+group_args.add_argument('--breath_mono', help='Single colour breathing, opt speed (1-4), RGB hex value', action='store_true')
+parser.add_argument('--config', help='Prints the current configuration', action='store_true')
+parser.add_argument('--raw_config', help='Prints the current raw configuration', action='store_true')
+args = parser.parse_known_args()
+# Not the best, but it's working.
+extra = args[1]  # Extra parameter for option customization
+args = args[0]
 
 devices = hid.enumerate(model_O_ids.vid, model_O_ids.pid)
 if not len(devices):
@@ -49,22 +65,130 @@ try:
     with hid.Device(path=device_path) as h:
         version_req = b'\x05\x11\x00\x00\x00\x00'
         res = h.send_feature_report(version_req)
-        print(f'req res: {res}')
         config = h.get_feature_report(0x04, 520)
-        print(f'config: {config.hex(sep=" ")}')
         mor = GloriousModelORecord(config)
-        print(f'mor: {mor}')
         update = False
-        if args.on:
-            if mor.effect != GloriousEffect.SINGLE_COLOUR:
-                mor.effect = GloriousEffect.SINGLE_COLOUR
-                mor.single_rgb = 0x330066
-                update = True
-        elif args.off:
-            if mor.effect != GloriousEffect.OFF:
-                mor.effect = GloriousEffect.OFF
-                update = True
+        if args.off:
+            mor.effect = GloriousEffect.OFF
+            update = True
+        elif args.glorious:
+            if len(extra):
+                try:
+                    mor.glorious_direction = EffectDirection(int(extra[0]))
+                    if len(extra) > 1:
+                        mor.glorious_speed = int(extra[1])
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.GLORIOUS
+            update = True
+        elif args.single:
+            if len(extra):
+                try:
+                    mor.single_rgb = int(extra[0], 16)
+                    if len(extra) > 1:
+                        mor.single_rgb_brightness = int(extra[1])
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.SINGLE_COLOUR
+            update = True
+        elif args.breath:
+            if len(extra):
+                try:
+                    mor.breath_speed = int(extra[0])
+                    if len(extra) > 1:
+                        values = []
+                        count = len(extra) - 1
+                        if count > 7:
+                            count = 7
+                        for pos in range(1, count + 1):
+                            values.append(int(extra[pos], 16))
+                        mor.breath_rgbs = values
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.BREATHING
+            update = True
+        elif args.tail:
+            if len(extra):
+                try:
+                    mor.tail_brightness = int(extra[0])
+                    if len(extra) > 1:
+                        mor.tail_speed = int(extra[1])
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.TAIL
+            update = True
+        elif args.seamless:
+            if len(extra):
+                try:
+                    mor.x_speed = int(extra[0])
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.SEAMLESS_BREATHING
+            update = True
+        elif args.six:
+            if len(extra) == 6:
+                try:
+                    values = []
+                    for pos in range(6):
+                        values.append(int(extra[pos], 16))
+                    mor.constant_rgbs = values
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.CONSTANT_RGB
+            update = True
+        elif args.rave:
+            if len(extra):
+                try:
+                    mor.rave_brightness = int(extra[0])
+                    if len(extra) > 1:
+                        mor.rave_speed = int(extra[1])
+                    if len(extra) == 4:
+                        values = []
+                        for pos in range(2, 4):
+                            values.append(int(extra[pos], 16))
+                        mor.rave_rgbs = values
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.RAVE
+            update = True
+        elif args.random:
+            if len(extra):
+                try:
+                    mor.random_speed = int(extra[0])
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.RANDOM
+            update = True
+        elif args.wave:
+            try:
+                mor.wave_brightness = int(extra[0])
+                if len(extra) > 1:
+                    mor.wave_speed = int(extra[1])
+            except ValueError:
+                pass
+            mor.effect = GloriousEffect.WAVE
+            update = True
+        elif args.breath_mono:
+            if len(extra):
+                try:
+                    mor.single_breath_speed = int(extra[0])
+                    if len(extra) > 1:
+                        mor.single_breath_rgb = int(extra[1], 16)
+                except ValueError:
+                    pass
+            mor.effect = GloriousEffect.SINGLE_BREATHING
+            update = True
         if update:
             res = h.send_feature_report(mor.record)
+        if args.config:
+            config = h.get_feature_report(0x04, 520)
+            mor = GloriousModelORecord(config)
+            print(mor)
+        if args.raw_config:
+            config = h.get_feature_report(0x04, 520)
+            base = 0
+            while base < len(config):
+                print(config[base:base + 16].hex(' '))
+                base += 16
 except hid.HIDException as err:
     print(f'Error: {err}')
